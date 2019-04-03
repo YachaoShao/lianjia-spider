6953cce5720b7b215a341c2dd4fb670e16ba78e7
Yachao Shao <y.shao@fz-juelich.de>
2019-04-03 17:12:54 +0200

    Transfer address to location.

--- merge_ershou.py
+++ merge_ershou.py
@@ -11,12 +11,8 @@ def merge(file_path):
     files = os.listdir(file_path)
     print(u'共发现%s个CSV文件'% len(files))
     print(u'正在处理............')
-    fout = open("second_hand_price_beijing_with_position.csv","wb")
+    fout = open("ershou_merged_20190401.csv","wb")
     for i in files:
-        # if os.stat(i).st_size == 0:
-        #     pass
-        # else:
-            # fr = open(i,'r').read()
             print('processing file：'+i)
             file_path = path +'/'+i
             file_input = open(file_path,'rb')
@@ -29,13 +25,10 @@ def merge(file_path):
             fout.close
     print(u'合并完毕！')
 
-def delete_duplicates(merged_file):
-    reader = pd.read_csv(merged_file)
-    datalist = reader.drop_duplicates()
-    datalist.to_csv(merged_file)
+
 
 if __name__ == '__main__':
-    path = '/home/yachao/github/lianjia-spider/data/ershou/bj/20190326'
+    path = '/home/yachao/github/lianjia-spider/data/ershou/bj/20190401'
     merge(path)
-    delete_duplicates('second_hand_price_beijing.csv')
+
         
